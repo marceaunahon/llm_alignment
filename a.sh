d410f5b1b3f09b0044066dbb5466748341d50ac9
marceaunahon <uus79sn@jean-zay2.idris.fr>
2024-07-29 15:04:22 +0200

    jz

--- a.sh
+++ a.sh
@@ -14,4 +14,4 @@ conda deactivate # desactiver les environnements herites par defaut
 module load cpuarch/amd # selectionner les modules compiles pour AMD
 module load pytorch-gpu/py3/1.12.1 # charger les modules
 set -x # activer l’echo des commandes
-srun python -u src.evaluate --experiment-name "jz_first_test" --dataset "kill" --model "google/flan-t5-small" --question-types "ab"  --eval-nb-samples 1 --dataset-folder "paperlaws"  # executer son script
+srun python -m src.evaluate --experiment-name "jz_first_test" --dataset "kill" --model "google/flan-t5-small" --question-types "ab"  --eval-nb-samples 1 --dataset-folder "paperlaws"  # executer son script

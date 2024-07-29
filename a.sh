9e1e5d22dcc32b539042f3cc1543d1617ed26cf5
Marceau <marceau.nahon@gmail.com>
2024-07-29 15:11:38 +0200

    requirements

--- a.sh
+++ a.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=Test1  # nom du job
-#SBATCH --output=Test1%j.out # fichier de sortie (%j = job ID)
-#SBATCH --error=Test1%j.err # fichier d’erreur (%j = job ID)
+#SBATCH --output=exps/Test1%j.out # fichier de sortie (%j = job ID)
+#SBATCH --error=exps/Test1%j.err # fichier d’erreur (%j = job ID)
 #SBATCH --nodes=1 # reserver 2 nœud
 #SBATCH --ntasks=1 # reserver 16 taches (ou processus)
 #SBATCH --gres=gpu:1 # reserver 8 GPU par noeud
@@ -14,4 +14,5 @@ conda deactivate # desactiver les environnements herites par defaut
 module load cpuarch/amd # selectionner les modules compiles pour AMD
 module load pytorch-gpu/py3/1.12.1 # charger les modules
 set -x # activer l’echo des commandes
+pip install -r requirements.txt
 srun python -m src.evaluate --experiment-name "jz_first_test" --dataset "kill" --model "google/flan-t5-small" --question-types "ab"  --eval-nb-samples 1 --dataset-folder "paperlaws"  # executer son script

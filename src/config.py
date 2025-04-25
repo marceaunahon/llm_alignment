9db358fdaea6112c56aed79a23f5d2a502173a8b
Marceau <marceau.nahon@gmail.com>
2025-04-25 15:06:13 +0200

    new config - responses2

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ PATH_API_KEYS = ROOT / "api_keys"
 PATH_HF_CACHE = ROOT / "cache"
 PATH_OFFLOAD = ROOT / "offload"
 
-PATH_RESULTS = ROOT / "data/responses"
+PATH_RESULTS = ROOT / "data/responses2"
 PATH_QUESTION_TEMPLATES = ROOT / "data/question_templates"
 PATH_RESPONSE_TEMPLATES = ROOT / "data/response_templates"
 PATH_RULES = ROOT / "data/rules"
